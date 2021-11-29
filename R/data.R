681f3949588a0eb3136478968252f3365ec8bacd
Christian Knudsen <christian.knudsen@gmail.com>
2021-11-29 13:37:36 +0100

    rasgu

--- R/data.R
+++ R/data.R
@@ -1,24 +1,3 @@
-#' Navne på ugedage.
+#' A character string
 #'
-#' Full baby name data provided by the SSA. This includes all names with at
-#' least 5 uses.
-#'
-#' @format A data frame with five variables: \code{year}, \code{sex},
-#'   \code{name}, \code{n} and \code{prop} (\code{n} divided by total number
-#'   of applicants in that year, which means proportions are of people of
-#'   that sex with that name born in that year).
-#'   @name ugedate
-#'   @export
-"ugedage"
-
-#' datoer på samlinger.
-#'
-#' Full baby name data provided by the SSA. This includes all names with at
-#' least 5 uses.
-#' @format A data frame with five variables: \code{year}, \code{sex},
-#'   \code{name}, \code{n} and \code{prop} (\code{n} divided by total number
-#'   of applicants in that year, which means proportions are of people of
-#'   that sex with that name born in that year).
-#' @name samlinger_dato
-#' @export
 "samlinger_dato"
