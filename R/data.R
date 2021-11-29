dda00e6e5777b75f79712151c132410bc3e80513
Christian Knudsen <christian.knudsen@gmail.com>
2021-11-29 13:11:52 +0100

    samlinger_dato forsøgt gjort exporterbare

--- R/data.R
+++ R/data.R
@@ -13,7 +13,8 @@
 #'
 #' Full baby name data provided by the SSA. This includes all names with at
 #' least 5 uses.
-#'
+#' @name samlinger_dato
+#' @export
 #' @format A data frame with five variables: \code{year}, \code{sex},
 #'   \code{name}, \code{n} and \code{prop} (\code{n} divided by total number
 #'   of applicants in that year, which means proportions are of people of
