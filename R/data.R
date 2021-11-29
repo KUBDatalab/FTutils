0db4f85249bc968d209167d94be26ffc004c7c4f
Christian Knudsen <christian.knudsen@gmail.com>
2021-11-29 12:55:44 +0100

    data.r tilføjet - dokumentation på ugedage startet.

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Navne på ugedage.
+#'
+#' Full baby name data provided by the SSA. This includes all names with at
+#' least 5 uses.
+#'
+#' @format A data frame with five variables: \code{year}, \code{sex},
+#'   \code{name}, \code{n} and \code{prop} (\code{n} divided by total number
+#'   of applicants in that year, which means proportions are of people of
+#'   that sex with that name born in that year).
+"ugedage"
