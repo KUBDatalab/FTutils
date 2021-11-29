d1ad88cf60726cf0cd988ba6b3f99ee78633f7b4
Christian Knudsen <christian.knudsen@gmail.com>
2021-11-29 12:23:55 +0100

    Dokumentation for samlinger_dato tilføjet

--- data-raw/samlinger_dato.R
+++ data-raw/samlinger_dato.R
@@ -1,14 +1,15 @@
-#' Ugedagenes navne på dansk
-#'
-#'
-#' @format A data frame with 53940 rows and 10 variables:
-#' \describe{
-#'   \item{price}{price, in US dollars}
-#'   \item{carat}{weight of the diamond, in carats}
-#'   ...
-#' }
-#'
+#'  Returner samlinger
+#' @description
+#'  Returnerer samling for en given dato
+#' @title sammlinger_dato
+#' @details samlinger_dato
+#' @param x en teksstreng formatteret som YYYY-MM-DD
+#' @name samlinger_dato
 #' @export
+#'
+#' @examples
+#' # Returnerer 20201 for 1. samling i 2020:
+#' which_assembly("2021-04-01")
 #'
 require(readr)
 
@@ -16,3 +17,17 @@ samlinger_dato <- read_csv("data-raw/samlinger_dato.csv") %>%
   mutate(period = interval(Startdato, Slutdato))
 
 usethis::use_data(samlinger_dato, overwrite = TRUE)
+
+
+#'  Returner samlinger
+#' @description
+#'  Returnerer samling for en given dato
+#' @title which_assembly
+#' @details which_assembly
+#' @param x en teksstreng formatteret som YYYY-MM-DD
+#' @name which_assembly
+#' @export
+#'
+#' @examples
+#' # Returnerer 20201 for 1. samling i 2020:
+#' which_assembly("2021-04-01")
