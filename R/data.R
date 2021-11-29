231a1f120fd682e3a4d6249ad4403f1ea83cca8b
Christian Knudsen <christian.knudsen@gmail.com>
2021-11-29 12:57:47 +0100

    dokumentation samlinger_dato opstartet

--- R/data.R
+++ R/data.R
@@ -8,3 +8,14 @@
 #'   of applicants in that year, which means proportions are of people of
 #'   that sex with that name born in that year).
 "ugedage"
+
+#' datoer på samlinger.
+#'
+#' Full baby name data provided by the SSA. This includes all names with at
+#' least 5 uses.
+#'
+#' @format A data frame with five variables: \code{year}, \code{sex},
+#'   \code{name}, \code{n} and \code{prop} (\code{n} divided by total number
+#'   of applicants in that year, which means proportions are of people of
+#'   that sex with that name born in that year).
+"samlinger_dato"
