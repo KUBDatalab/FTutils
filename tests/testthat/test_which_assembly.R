242133bea97da6088b15c809cdaed30fa214ace4
Christian Knudsen <christian.knudsen@gmail.com>
2021-11-29 09:28:53 +0100

    Yderligere test af which_assembly

--- tests/testthat/test_which_assembly.R
+++ tests/testthat/test_which_assembly.R
@@ -4,3 +4,7 @@ test_that("which_assembly returnerer korrekt",{
   expect_equal(which_assembly("2021-04-01"), 20201)
 }
           )
+
+test_that("which_assembly returnerer korrekt antal samlinger",{
+  expect_equal(length(which_assembly("1974-06-20:2016-04-30")), 58)
+})
