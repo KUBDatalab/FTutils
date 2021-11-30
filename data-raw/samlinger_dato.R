d16f1df84e38b09dc09b48093c65ba40188f28aa
Christian Knudsen <christian.knudsen@gmail.com>
2021-11-30 09:55:41 +0100

    vi prøver igen.

--- data-raw/samlinger_dato.R
+++ data-raw/samlinger_dato.R
@@ -1,3 +1,8 @@
 ## code to prepare `samlinger_dato` dataset goes here
 samlinger_dato <- read_csv("data-raw/samlinger_dato.csv")
 usethis::use_data(samlinger_dato, overwrite = TRUE)
+
+#' Cool data which is used internally in the package
+#' @name samlinger_dato
+#' @export
+"samlinger_dato"
