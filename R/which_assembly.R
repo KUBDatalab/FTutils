d2b1fd753951c90a2956d8d8b50e0a386254ade7
Christian Knudsen <christian.knudsen@gmail.com>
2022-04-20 16:41:00 +0200

    Når vi importerer biblioteker - så skal vi gøre det med eksempelvis: #' @importFrom lubridate %within% #' @import lubridate i scriptet der definerer funktionen.

--- R/which_assembly.R
+++ R/which_assembly.R
@@ -6,33 +6,34 @@
 #' @param x en teksstreng formatteret som YYYY-MM-DD
 #' @name which_assembly
 #' @export
-#'
+#' @importFrom lubridate %within%
+#' @import lubridate
 #' @examples
 #' # Returnerer 20201 for 1. samling i 2020:
+#' library(FTutils)
 #' which_assembly("2021-04-01")
 
-library(tidyverse)
-library(lubridate)
+require(tidyverse)
+require(lubridate)
 library(readxl)
+library(dplyr)
 
-
-data("samlinger_dato")
-assemb <- samlinger_dato
 
 which_assembly <- function(date){
+  assemb <- FTutils::samlinger_dato
   if (is.character(date) == FALSE) {
     stop("Error - Remember that the date should be in quotes and in a format like this: 'YYYY-MM-DD'")
-  } else if (str_detect(date, "^\\d{4}-\\d{2}-\\d{2}$") == TRUE) {
+  } else if (stringr::str_detect(date, "^\\d{4}-\\d{2}-\\d{2}$") == TRUE) {
     matched_samling <- assemb %>%
-      mutate(match = (ymd(date) %within% period)) %>%
-      filter(match == TRUE) %>%
-      pull(Samling)
+      dplyr::mutate(match = (ymd(date) %within% period)) %>%
+      dplyr::filter(match) %>%
+      dplyr::pull(Samling)
     matched_samling
   } else if(str_detect(date, "^\\d{4}-\\d{2}-\\d{2}:\\d{4}-\\d{2}-\\d{2}$") == TRUE) {
     periods <- str_split(date, ":")
     matched_period <-assemb %>%
       mutate(interval_over = int_overlaps(interval(ymd(periods[[1]][1]), ymd(periods[[1]][2])), period)) %>%
-      filter(interval_over == TRUE) %>%
+      dplyr::filter(interval_over) %>%
       pull(Samling)
 
     matched_period
